cbdf2e90548f8a3a5486df9accdaf4238d8ee9e6
LiamGunning07 <liamgunning07@gmail.com>
2023-11-21 14:10:24 -0700

    "Linted"

--- countOnly.js
+++ countOnly.js
@@ -10,7 +10,7 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (itemsToCount[item]) { 
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -20,18 +20,18 @@ const countOnly = function(allItems, itemsToCount) {
   }
 
   return results;
-}
+};
 const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
 
 
 
