8d88872aceeb712a81d1bfa0b5970b0a511fa159
LiamGunning07 <liamgunning07@gmail.com>
2023-11-23 17:05:19 -0700

    "Linted"

--- findKey.js
+++ findKey.js
@@ -10,20 +10,20 @@ const assertEqual = function(actual, expected) {
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {
-    return key;
+      return key;
     }
   }
-return undefined;
+  return undefined;
 };
 
- const result1 = findKey({
+const result1 = findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
 
 const result2 = findKey({
   "Blue Hill": { stars: 1 },
@@ -32,7 +32,7 @@ const result2 = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 3)
+}, x => x.stars === 3);
 
 assertEqual('noma', result1);
 assertEqual('noma', result2);
