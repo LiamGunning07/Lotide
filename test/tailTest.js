fcf4ad8806210965e60bec399fdc289612f9aa2c
LiamGunning07 <liamgunning07@gmail.com>
2023-12-05 16:43:25 -0700

    "Tests updated"

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,6 +5,6 @@ console.log(tail([1,2,3,4]));
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-const words = [];
+const words = ['hi', 'john', 'deere'];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
