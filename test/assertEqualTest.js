08cb1a7c771296edef261b6cb209f6b6fe86c56f
LiamGunning07 <liamgunning07@gmail.com>
2023-12-05 16:25:47 -0700

    Updated with module.export

---
+++ test/assertEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual(23, 45);
---
+++ test/headTest.js
@@ -0,0 +1,4 @@
+const head = require(`../head`);
+const assertEqual = require('../assertEqual');
+assertEqual(head([]));
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
