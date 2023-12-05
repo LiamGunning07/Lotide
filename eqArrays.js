30dea7ff25dfbe6fe08388eea722323bf33c3a15
LiamGunning07 <liamgunning07@gmail.com>
2023-12-05 16:52:50 -0700

    "Updated with imports and exports with tests relocated"

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
- const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed:${actual} !== ${expected}`);
-  }
-}; 
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,11 +13,7 @@ const eqArrays = function(array1, array2) {
 
   return true;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
+module.exports = eqArrays;
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 
