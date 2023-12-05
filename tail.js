60756c69213756aaa1aad4616fb6449e251fa12f
LiamGunning07 <liamgunning07@gmail.com>
2023-12-05 16:36:41 -0700

    "Updated with module.export and tests relocated"

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed:${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   let result = array.slice(1);
   return result;
 };
-console.log(tail([1,2,3,4]));
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-const words = [];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
