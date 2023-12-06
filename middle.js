1baa0e5c757877467cba6cb634078fc9246b2ec4
LiamGunning07 <liamgunning07@gmail.com>
2023-12-06 09:06:58 -0700

    "Updated with imports and exports; tests moved"

--- middle.js
+++ middle.js
@@ -1,30 +1,5 @@
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    console.log(`❌❌❌ Assertion Failed:${array1} !== ${array2}`);
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      console.log(`❌❌❌ Assertion Failed:${array1} !== ${array2}`);
-      return false;
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed:${array1} === ${array2}`);
-  return true;
-};
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const assertArraysEqual = require(`./assertArraysEqual`);
+const eqArrays = require(`./eqArrays`);
 
 const middle = function(array) {
   const length = array.length;
@@ -44,16 +19,4 @@ const middle = function(array) {
     return [array[middleIndex]];
   }
 };
-
-// Example usage:
-console.log(middle([1, 2, 3]));      // Output: [2]
-console.log(middle([1, 2, 3, 4, 5])); // Output: [3]
-console.log(middle([1, 2, 3, 4]));    // Output: [2, 3]
-console.log(middle([1]));             // Output: []
-console.log(middle([1, 2]));          // Output: []
-// Example usage:
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
