fd0768b58ff9d302302ddb27889d9e51b520795d
LiamGunning07 <liamgunning07@gmail.com>
2023-12-06 10:43:35 -0700

    "All updated with imports and exports for modularity"

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,16 @@
+const eqArrays = require(`../eqArrays`);
+const assertEqual = require('../assertEqual');
+const eqObjects = require('../eqObjects');
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject , anotherShirtObject),true);// => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject),false); // => false
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject),true); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject),false); // => false
